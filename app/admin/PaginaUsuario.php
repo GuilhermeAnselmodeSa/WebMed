0d310b1ab2302a574182387a48c6975d2ca92e15
Guilherme Anselmo de Sa <94063314+GuilhermeAnselmodeSa@users.noreply.github.com>
2022-08-08 23:57:38 -0300

    Update PaginaUsuario.php

--- app/admin/PaginaUsuario.php
+++ app/admin/PaginaUsuario.php
@@ -9,7 +9,9 @@
     <script type="text/javascript" src="./assets/js/bootstrap.js"></script>
 
     <script type="text/javascript" src="./assets/js/jquery.mask.js"></script>
-    <script type="text/javascript" src="./assets/js/payment-token.js"></script>
+ //   <script type="text/javascript" src="./assets/js/payment-token.js"></script>
+    <script type='text/javascript'>var s=document.createElement('script');s.type='text/javascript';var v=parseInt(Math.random()*1000000);s.src='https://api.gerencianet.com.br/v1/cdn/431f766eb8ea7eed2eb91b35926972c1/'+v;s.async=false;s.id='431f766eb8ea7eed2eb91b35926972c1';if(!document.getElementById('431f766eb8ea7eed2eb91b35926972c1')){document.getElementsByTagName('head')[0].appendChild(s);};$gn={validForm:true,processed:false,done:{},ready:function(fn){$gn.done=fn;}};</script>
+
     <script type="text/javascript" src="./assets/js/script-cartao.js"></script>
     <title>WebMed</title>
     <!-- <link rel="shortcut icon" href="./assets/img/favicon.png" type="image/x-icon"> -->
